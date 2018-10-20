9fb61713c4ec123834942ee9816e80e9f608f66b
Robert Chen <robertchen234@gmail.com>
2018-10-20 21:20:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,15 @@
 def badge_maker(name)
   "Hello, my name is #{badge}."
 end
+
+def batch_badge_creator(attendees)
+  badge_maker(name)
+end 
+
+def assign_rooms(attendees)
+  @room += 1
+  "Hello, #{attendees}! You'll be assigned to room #{room}!"
+end 
+
+def printer(attendees)
+  puts 
