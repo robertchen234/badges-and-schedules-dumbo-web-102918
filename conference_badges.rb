bc88190d6223fbf303d1b1e94405135beb64a110
Robert Chen <robertchen234@gmail.com>
2018-10-20 21:37:02 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,17 +6,17 @@ end
 
 def batch_badge_creator(attendees)
   list_attendees = []
-  attendees.each { |attendee| list_attendees << "Hello, my name is #{attendee}." }
+  attendees.each{ |attendee| list_attendees << "Hello, my name is #{attendee}." }
   list_attendees
 end 
 
 def assign_rooms(attendees)
   assigned = []
-  attendees.each_with_index { |attendee, room| assigned << "Hello, #{attendee}! You'll be assigned to room #{room+1}!" }
+  attendees.each_with_index{ |attendee, room| assigned << "Hello, #{attendee}! You'll be assigned to room #{room+1}!" }
   assigned
 end 
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  batch_badge_creator(attendees).each{ |badge| puts badge }
+  assign_rooms(attendees).each{ |room| puts room }
 end
